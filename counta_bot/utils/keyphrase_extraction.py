2a16b4f567fae638d2823c88d09a426178a227b7
Joshua Sheppard <joshua.sheppard@b-4551.home>
2022-08-30 00:12:54 +0100

    update keyphrase extraction module with keyphrase vectoriser

--- counta_bot/utils/keyphrase_extraction.py
+++ counta_bot/utils/keyphrase_extraction.py
@@ -1,11 +1,16 @@
 from keybert import KeyBERT
+from keyphrase_vectorizers import KeyphraseCountVectorizer
 
 kb = KeyBERT()
 
-def exctract_keyphrase(doc, n_gram=2, n_kp=3):
-    #kp = kb.extract_keywords(doc, keyphrase_ngram_range=(1, 3), stop_words=None, use_mmr="True", diversity=0.2)
-    #kp = kb.extract_keywords(doc, keyphrase_ngram_range=(1, n_kp), stop_words="english", use_maxsum="True", top_n=5)
+def extract_keyphrase(doc, n_gram=3, n_kp=3, use_mmr="False", use_maxsum="False"):
 
-    kp = kb.extract_keywords(doc, keyphrase_ngram_range=(0, n_gram), stop_words="english")
+    #kp = kb.extract_keywords(doc, keyphrase_ngram_range=(0, n_gram), stop_words="english", diversity=0.2, vectorizer=KeyphraseCountVectorizer())
+    kp = kb.extract_keywords(doc, vectorizer=KeyphraseCountVectorizer())
+
+    #kw_model.extract_keywords(doc, vectorizer=KeyphraseCountVectorizer())
 
     return [i[0] for i in kp[0:n_kp]]
+
+test = "The environmental impact of aviation in the UK is increasing due to new terminals at Heathrow airport"
+print(extract_keyphrase(test))
